1f89a27bfff9bb827effb01dcf3a469384be18dc
TodorovaTeodora <57221159+TodorovaTeodora@users.noreply.github.com>
2023-05-26 08:38:33 +0300

    Update MailSender.java

--- src/main/java/com/mockitotutorial/happyhotel/booking/MailSender.java
+++ src/main/java/com/mockitotutorial/happyhotel/booking/MailSender.java
@@ -3,7 +3,7 @@ package com.mockitotutorial.happyhotel.booking;
 public class MailSender {
 
 	public void sendBookingConfirmation(String bookingId) {
-		// TODO Not done yet. John Smith to implement!
+		// TODO Not done yet. 
 		throw new UnsupportedOperationException("Not implemented yet");
 	}
 
